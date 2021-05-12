1ea8b4030943b127330327559398460543836a9a
Fernando Rodrigues <34346597+nandolrs@users.noreply.github.com>
2021-05-12 19:30:26 -0300

    primeiro commit

---
+++ src/main/java/com/nandolrs/ifrsdev1appv1r1/repositorios/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.nandolrs.ifrsdev1appv1r1.repositorios;
+
+import org.springframework.data.repository.CrudRepository;
+import com.nandolrs.ifrsdev1appv1r1.dados.Category;
+
+public interface CategoryRepository extends CrudRepository<Category, Integer> {
+
+}
---
+++ src/test/java/com/nandolrs/ifrsdev1appv1r1/IfrsDev1Appv1r1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nandolrs.ifrsdev1appv1r1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfrsDev1Appv1r1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
